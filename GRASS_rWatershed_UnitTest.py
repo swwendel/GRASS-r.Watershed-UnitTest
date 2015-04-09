89fa5972806bb80b93fee17398e223248ffd5fda
Stephanie Wendel <swendel@esri.com>
2015-04-08 23:45:39 -0400

    Version 1.2
    Updated script to address issues #4, #5, #6. See those issues for notes.

--- GRASS_rWatershed_UnitTest.py
+++ GRASS_rWatershed_UnitTest.py
@@ -6,8 +6,8 @@ Purpose:    This script is to demonstrate a unit test for GRASS's r.Watershed
 
 Author:     Stephanie Wendel - sawendel
 GRASS:      7.1.svn-r64925-36
-Version:    1.1
-Modified:   3/3/2015
+Version:    1.2
+Modified:   4/8/2015
 Copyright:  (c) sawendel 2015
 Licence:    GNU GPL
 """
@@ -35,19 +35,67 @@ class TestWatershed(grass.gunittest.TestCase):
         """Remove the outputs created from the watershed module after each test
         is run."""
         self.runModule('g.remove', flags='f', type='raster',
-            name='test_accumulation,test_basin')
+            name='test_accumulation,test_drainage,test_basin,test_stream,test_halfbasin,test_lengthslope,test_slopesteepness')
+        self.runModule('g.remove', flags='f', type='raster',
+            name='test_stream_4,test_lengthslope_4,test_stream_8,test_lengthslope_8')
 
     def test_OutputCreated(self):
         """Test to see if the outputs are created"""
         #run the watershed module
         self.assertModule('r.watershed', elevation='elevation',
             threshold='10000', accumulation='test_accumulation',
-            basin='test_basin')
+            drainage='test_drainage', basin='test_basin', stream='test_stream',
+            half_basin='test_halfbasin', length_slope='test_lengthslope',
+            slope_steepness='test_slopesteepness')
         #check to see if accumulation output is in mapset
         self.assertRasterExists('test_accumulation',
             msg='test_accumulation output was not created')
+        #check to see if drainage output is in mapset
+        self.assertRasterExists('test_drainage',
+            msg='test_drainage output was not created')
+        #check to see if basin output is in mapset
         self.assertRasterExists('test_basin',
             msg='test_basin output was not created')
+        #check to see if stream output is in mapset
+        self.assertRasterExists('test_stream',
+            msg='test_stream output was not created')
+        #check to see if half.basin output is in mapset
+        self.assertRasterExists('test_halfbasin',
+            msg='test_halfbasin output was not created')
+        #check to see if length.slope output is in mapset
+        self.assertRasterExists('test_lengthslope',
+            msg='test_lengthslope output was not created')
+        #check to see if slope.steepness output is in mapset
+        self.assertRasterExists('test_slopesteepness',
+            msg='test_slopesteepness output was not created')
+
+    def test_fourFlag(self):
+        """Test the -4 flag to see if the stream and slope lengths are
+        approximately the same as the outputs from the default module run"""
+        #Run module with default settings
+        self.assertModule('r.watershed', elevation='elevation',
+            threshold='10000', stream='test_stream_8',
+            length_slope='test_lengthslope_8')
+        #Run module with flag 4
+        self.assertModule('r.watershed', flags='4', elevation='elevation',
+            threshold='10000', stream='test_stream_4',
+            length_slope='test_lengthslope_4')
+        #Use the assertRastersNoDifference with precsion 10 to see if close
+        #Compare stream output
+        self.assertRastersNoDifference('test_stream_4', 'test_stream_8', 10)
+        #Compare length_slope output
+        self.assertRastersNoDifference('test_lengthslope_4',
+            'test_lengthslope_8', 10)
+
+    def test_watershedThreadholdfail(self):
+        """Check to see if it will allow for a threshold of 0 or a negative"""
+        self.assertModuleFail('r.watershed', elevation='elevation', threshold='0',
+            stream='test_stream2', msg='Threshold value of 0 considered valid.')
+        self.assertModuleFail('r.watershed', elevation='elevation',
+            threshold='-1', stream='test_stream3',
+            msg='Threshold value of 0 considered valid.')
+
+
 
 if __name__ == '__main__':
     test()
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Features
 
-* version 1.1
+* version 1.2
 * Tests the r.watershed module of GRASS GIS.
 
 
